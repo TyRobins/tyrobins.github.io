ca3310c0747ba492a1cd453e91e0a1adb6b82dcd
TyRobins <tylerm.p.robinson@gmail.com>
2024-09-09 23:10:51 -0600

    9.9.24

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ document.addEventListener("DOMContentLoaded", () => {
 
     for (const image of images){
         console.log(image)
-        fetch("https://dog.ceo/api/breeds/image/random")
+        fetch("")
         .then(response => response.json())
         .then(data => {
             image.src = data.message;
